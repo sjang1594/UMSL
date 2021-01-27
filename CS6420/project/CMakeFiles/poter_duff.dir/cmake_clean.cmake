beb46dfd4a5ead12883dc85f28f659afc9856f70
Seungho Jang <sjang1594@gmail.com>
2021-01-26 22:02:33 -0600

    adding cmake & cmake

---
+++ CS6420/project/CMakeFiles/poter_duff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poter_duff.dir/porter_duff.cpp.o"
+  "poter_duff.pdb"
+  "poter_duff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poter_duff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
