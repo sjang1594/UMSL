95ddaa54b5bfb461a50e2a49b5e9195be2deb320
Seungho Jang <sjang1594@gmail.com>
2021-01-26 20:37:56 -0600

    add CMakeLists.txt

---
+++ CS6420/Project 1/cmake-build-debug/CMakeFiles/poter_duff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poter_duff.dir/porter_duff.cpp.o"
+  "poter_duff"
+  "poter_duff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poter_duff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
